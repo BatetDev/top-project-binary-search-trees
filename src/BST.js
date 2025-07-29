c95c2317ed8a1779c05cd747e47082483657215a
fr4nbtt <batet.francisco@gmail.com>
2025-07-29 19:58:31 -0300

    feat: Implement buildTree for balanced BST

--- src/BST.js
+++ src/BST.js
@@ -8,13 +8,40 @@ class Node {
   }
 }
 
-export class Tree {
-  constructor(array) {
-    this.root = this.buildTree(array);
+export class BST {
+  constructor(values) {
+    this.root = this.buildTree(values);
   }
 
-  buildTree(array) {
-    // temp - test
-    return new Node(array[0]);
+  buildTree(values) {
+    if (!values || values.length === 0) return null;
+
+    const cleaned = [...new Set(values)].sort((a, b) => a - b);
+    const midIndex = Math.floor(cleaned.length / 2);
+    const midValue = cleaned[midIndex];
+
+    const node = new Node(midValue);
+
+    const leftHalf = cleaned.slice(0, midIndex);
+    const rightHalf = cleaned.slice(midIndex + 1);
+
+    node.left = this.buildTree(leftHalf);
+    node.right = this.buildTree(rightHalf);
+
+    return node;
   }
 }
+
+// For visualization
+export const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 
-import { Tree } from "./Tree.js";
+import { BST, prettyPrint } from "./BST.js";
 
-const tree = new Tree([1, 2, 3]);
-console.log(tree.root);
+const tree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(tree.root);
